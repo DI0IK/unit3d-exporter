6001ec0cc7190d9bee8eeaeeafffec1a4cde8a22
Dominik Stahl <dominik@samdj.de>
2026-02-08 16:12:11 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:alpine AS base
+
+FROM base AS builder
+
+WORKDIR /app
+
+COPY package.json pnpm-lock.yaml /app/
+
+RUN pnpm install
+
+COPY tsconfig.json main.ts /app/
+
+RUN pnpm tsc
+
+FROM base AS runner
+
+WORKDIR /app
+
+COPY --from=builder /app/dist/main.js /app/
+COPY --from=builder /app/node_modules /app/node_modules
+
+CMD [ "node", "/app/main.js" ]
---
+++ main.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/metrics", async (_req, res) => {
+	let response = "";
+
+	const trackerList = (process.env.TRACKERS || "")
+		.split(";")
+		.map((i) => i.trim())
+		.filter((i) => i);
+
+	for (const tracker of trackerList) {
+		const api_key = process.env[`TRACKER_${tracker}_API_KEY`] || "";
+		const url = process.env[`TRACKER_${tracker}_URL`] || "";
+
+		if (!api_key.trim() || !url.trim()) continue;
+
+		try {
+			const data = await fetch(`${url}/api/user?api_token=${api_key}`);
+			const dataObject: { [key: string]: string | number } =
+				(await data.json()) as { [key: string]: string | number };
+			Object.entries(dataObject).forEach(([key, value]) => {
+				response += `tracker_unit3d_${key}{tracker="${tracker}"} = ${value}\n`;
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	}
+
+	res.send(response);
+});
+
+app.listen(3000, () => {
+	console.log("Metrics scraper running at 0.0.0.0:3000");
+});
